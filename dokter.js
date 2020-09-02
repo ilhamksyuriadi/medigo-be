ef6b36d5985bfeed27799f70b4cecd145b09f948
ilhamksyuriadi <ilhamksyuriadi@gmail.com>
2020-09-02 23:23:35 +0700

    done

--- dokter.js
+++ dokter.js
@@ -1,74 +1,112 @@
 let dokter = {
     dokter: [
         {
-            id: 01,
+            id: '01',
             name: 'Dr. Anto',
             work_hour: '09:00 - 16:00',
             rumah_sakit: 'Rs. Satya Negara',
-            specialist: 'umum'
+            specialist: 'umum',
+            penyakit: ['flu', 'demam', 'konsultasi umum', 'penyakit pada anak'],
+            keahlian: ['anak', 'penyakit umum', 'konsultasi kesehatan']
         },
         {
-            id: 02,
+            id: '02',
             name: 'Dr. Andrian',
             work_hour: '09:00 - 16:00',
             rumah_sakit: 'Rs. Mayapada',
-            specialist: 'umum'
+            specialist: 'umum',
+            penyakit: ['flu', 'demam', 'konsultasi umum', 'penyakit pada anak'],
+            keahlian: ['anak', 'penyakit umum', 'konsultasi kesehatan']
         },
         {
-            id: 03,
+            id: '03',
             name: 'Dr. Lina',
             work_hour: '09:00 - 16:00',
             rumah_sakit: 'Rs. Pertamina',
-            specialist: 'jantung'
+            specialist: 'jantung',
+            penyakit: ['lemah jantung', 'serangan jantung', 'nyeri pada dada'],
+            keahlian: ['syaraf', 'anastesi', 'bedah']
         },
         {
-            id: 04,
+            id: '04',
             name: 'Dr. Ashri',
             work_hour: '09:00 - 16:00',
             rumah_sakit: 'Rs. M Djamil',
-            specialist: 'tht'
+            specialist: 'tht',
+            penyakit: ['telinga berair', 'gangguan pendengaran', 'nyeri tenggorokan'],
+            keahlian: ['syaraf', 'telinga', 'hidung', 'tenggorokan']
         },
         {
-            id: 05,
+            id: '05',
             name: 'Dr. Romi',
             work_hour: '09:00 - 16:00',
             rumah_sakit: 'Rs. Santoso',
-            specialist: 'kulit'
+            specialist: 'kulit',
+            penyakit: ['alergi kulit', 'konsultasi perawatan kulit', 'cacar air'],
+            keahlian: ['kecantikan kulit', 'perawatan kulit']
         },
         {
-            id: 06,
+            id: '06',
             name: 'Dr. Mifta',
             work_hour: '09:00 - 16:00',
             rumah_sakit: 'Rs. Tentara',
-            specialist: 'umum'
+            specialist: 'umum',
+            penyakit: ['flu', 'demam', 'konsultasi umum', 'penyakit pada anak'],
+            keahlian: ['anak', 'penyakit umum', 'konsultasi kesehatan']
         },
         {
-            id: 07,
+            id: '07',
             name: 'Dr. Annisa',
             work_hour: '09:00 - 16:00',
             rumah_sakit: 'Rs. Siloam',
-            specialist: 'umum'
+            specialist: 'umum',
+            penyakit: ['flu', 'demam', 'konsultasi umum', 'penyakit pada anak'],
+            keahlian: ['anak', 'penyakit umum', 'konsultasi kesehatan']
         },
         {
-            id: 08,
+            id: '08',
             name: 'Dr. Cao',
             work_hour: '09:00 - 16:00',
             rumah_sakit: 'Rs. Royal Progress',
-            specialist: 'jantung'
+            specialist: 'jantung',
+            penyakit: ['lemah jantung', 'serangan jantung', 'nyeri pada dada'],
+            keahlian: ['syaraf', 'anastesi', 'bedah']
         },
         {
-            id: 09,
+            id: '09',
             name: 'Dr. Yurda',
             work_hour: '09:00 - 16:00',
             rumah_sakit: 'Rs. Hermina',
-            specialist: 'tht'
+            specialist: 'tht',
+            penyakit: ['telinga berair', 'gangguan pendengaran', 'nyeri tenggorokan'],
+            keahlian: ['syaraf', 'telinga', 'hidung', 'tenggorokan']
         },
         {
-            id: 10,
+            id: '10',
             name: 'Dr. Claudia',
             work_hour: '09:00 - 16:00',
             rumah_sakit: 'Rs. Mitra Keluarga',
-            specialist: 'kulit'
+            specialist: 'kulit',
+            penyakit: ['alergi kulit', 'konsultasi perawatan kulit', 'cacar air'],
+            keahlian: ['kecantikan kulit', 'perawatan kulit']
+        },
+        {
+            id: '11',
+            name: 'Dr. Anthopolo',
+            work_hour: '09:00 - 16:00',
+            rumah_sakit: 'Rs. Mitra Kantor',
+            specialist: 'gigi',
+            penyakit: ['gigi berlubang', 'amandel', 'nyeri paada gusi', 'pemasangan behel'],
+            keahlian: ['operasi gigi', 'perawatan behel', 'syaraf']
+        },
+        {
+            id: '12',
+            name: 'Dr. Zubay',
+            work_hour: '09:00 - 16:00',
+            rumah_sakit: 'Rs. Sejahtera',
+            specialist: 'gigi',
+            penyakit: ['gigi berlubang', 'amandel', 'nyeri paada gusi', 'pemasangan behel'],
+            keahlian: ['operasi gigi', 'perawatan behel', 'syaraf']
         }
     ]
 }
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
 let dokter = require('./dokter');
 
 app.get('/dokter', (req,res) => {
+    res.setHeader('Content-Type', 'application/json');
     res.json(dokter);
 })
 
@@ -17,6 +24,20 @@ app.get('/dokter/:specialist', (req,res) => {
     }
     const dokter_obj = {
         dokter_specialist: dokter_specialist
+    }
+    res.json(dokter_obj)
+})
+
+app.get('/dokter/:specialist/:id', (req,res) => {
+    let dokter_specialist = {};
+    let id = req.params.id;
+    for (let i = 0; i < dokter.dokter.length; i++) {
+        if (dokter.dokter[i].id == id) {
+            dokter_specialist = Object.assign(dokter.dokter[i])
+        }
+    }
+    const dokter_obj = {
+        dokter: dokter_specialist
     }
     res.json(dokter_obj)
 })
